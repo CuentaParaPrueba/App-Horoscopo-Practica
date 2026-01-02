9820b3aafbcc03bb2fb56bdb54338deef28256cc
johnncanani <johnncanani@gmail.com>
2026-01-01 19:31:52 -0500

    initial commit

---
+++ app/src/main/java/com/jcanani/horoscoapp/ui/home/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.jcanani.horoscoapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jcanani.horoscoapp.R
+import com.jcanani.horoscoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initUI()
+
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController= navHost.navController
+        binding.bottonNavView.setupWithNavController(navController)
+    }
+}
